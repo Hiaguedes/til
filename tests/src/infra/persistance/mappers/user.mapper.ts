b90083c802950fd4d51dae95f4947ade011cf6cf
Hiago Guedes <hiago@contasimples.com>
2025-12-08 21:51:57 -0300

    refactor: user mapper type

--- tests/src/infra/persistance/mappers/user.mapper.ts
+++ tests/src/infra/persistance/mappers/user.mapper.ts
@@ -2,13 +2,13 @@ import { User } from "../../../domain/entities/user";
 import { UserEntity } from "../entities/user.entity";
 
 export class UserMapper {
-    static toPersistanceEntity(user: any): UserEntity {
+    static toPersistanceEntity(user: User): UserEntity {
         const userEntity = new UserEntity();
         userEntity.id = user.Id;
         userEntity.name = user.Name;
         return userEntity;
     }
-    static toDomain(userEntity: UserEntity): any {
+    static toDomain(userEntity: UserEntity): User {
         return new User({ id: userEntity.id, name: userEntity.name });
     }
 }
